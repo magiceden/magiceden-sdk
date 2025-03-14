3668348de3594bc90fd521facccf6b881242af99
Matthew <19557547+mtleliever@users.noreply.github.com>
2025-03-14 16:01:33 -0400

    update TransactionStrategy comments

--- src/types/transactions.ts
+++ src/types/transactions.ts
@@ -1,6 +1,14 @@
 
 /**
  * Transaction strategy
+ * 
+ * SignAndSend: Sign and immediately send any transactions
+ * 
+ * SignSendAndConfirm: Sign and send transactions, then wait for confirmation
+ * 
+ * SignSendAndConfirm will be used by default if no strategy is provided.
+ * If you want to sign and send transactions async, use SignAndSend.
+ * This will allow you to continue executing some other code while the transactions are being sent, and you can check the status of the transactions later.
  */
 export enum TransactionStrategy {
   /**
